4b2ae499d96aef15f212189c832295fe04f83712
6Morpheus6 <wanton-decay@gmx.net>
2025-05-05 02:55:28 +0200

    script version 3.7

--- pinokio.js
+++ pinokio.js
@@ -1,28 +1,101 @@
 const os = require('os')
 module.exports = {
+  version: "3.7",
   title: "Vid2DensePose",
   icon: "icon.png",
   description: "Convert your videos to densepose and use it on MagicAnimate https://github.com/Flode-Labs/vid2densepose",
-  menu: async (kernel) => {
-    let installed = await kernel.exists(__dirname, "app", "env")
-    if (installed) {
-      let running = await kernel.running(__dirname, "start.json")
-      if (running) {
-        return [
-          { icon: "fa-solid fa-spin fa-circle-notch", text: "Running" },
-          { icon: "fa-solid fa-terminal", text: "Terminal", href: "start.json" }
-        ]
+  menu: async (kernel, info) => {
+    let installed = info.exists("app/env")
+    let running = {
+      install: info.running("install.js"),
+      start: info.running("start.js"),
+      update: info.running("update.js"),
+      reset: info.running("reset.js"),
+      link: info.running("link.js")
+    }
+    if (running.install) {
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
+      if (running.start) {
+        let local = info.local("start.js")
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        } else {
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        }
+      } else if (running.update) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Updating",
+          href: "update.js",
+        }]
+      } else if (running.reset) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Resetting",
+          href: "reset.js",
+        }]
+      } else if (running.link) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Deduplicating",
+          href: "link.js",
+        }]
       } else {
-        return [
-          { icon: "fa-solid fa-power-off", text: "start", href: "start.json", params: { fullscreen: true, run: true } },
-        ]
+        return [{
+          default: true,
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-solid fa-file-zipper",
+          text: "<div><strong>Save Disk Space</strong><div>Deduplicates redundant library files</div></div>",
+          href: "link.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
+          href: "reset.js",
+          confirm: "Are you sure you wish to reset the app?"
+
+        }]
       }
     } else {
-      if (os.platform() === 'win32') {
-        return [{ icon: "fa-solid fa-plug", text: "Install", href: "install_win.json", params: { run: true, fullscreen: true } }]
-      } else {
-        return [{ icon: "fa-solid fa-plug", text: "Install", href: "install.json", params: { run: true, fullscreen: true } }]
-      }
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
     }
   }
 }
